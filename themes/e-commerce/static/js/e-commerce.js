ef9bd42d6799e3209570c7240ece91637ed809ea
Le Tan Vu <vu.letan@bosch-si.com>
2017-04-14 13:58:32 +0800

    Synchronize code from dalafarm-v0

--- themes/e-commerce/static/js/e-commerce.js
+++ themes/e-commerce/static/js/e-commerce.js
@@ -30,8 +30,7 @@ $(document).ready(function(){
   var maxOffset = 300;
   if ($(window).scrollTop() >= maxOffset) {
     $('.navbar-default').addClass('navbar-shrink');
-  }
-  else {
+    } else {
     $('.navbar-default').removeClass('navbar-shrink');
   }
 });
@@ -49,11 +48,11 @@ $('.navbar-collapse ul li a').click(function() {
 // Async contact form
 $('form[id=contactForm]').submit(function(){
   $.post($(this).attr('action'), $(this).serialize(), function(res){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    if (res.code == "200")
-      $('form[id=contactForm] #success').show();
-    }).fail(function(){
+        $('form[id=contactForm] #error').hide();
+        $('form[id=contactForm] #success').show();
+        $('form[id=contactForm]').trigger("reset");
+        $('form[id=contactForm] .btn').prop('disabled', true);
+    }, "json").fail(function () {
     $('form[id=contactForm] #success').hide();
     $('form[id=contactForm] #error').hide();
     $('form[id=contactForm] #error').show();
