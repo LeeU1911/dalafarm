a6884716bc4640bf5b077ce0903f90aa1f9a7022
Le Tan Vu <vu.letan@bosch-si.com>
2017-04-22 16:31:53 +0800

    Put 3 products tile on the same row and make it as tall as the tallest tile

--- themes/e-commerce/static/js/e-commerce-other-page.js
+++ themes/e-commerce/static/js/e-commerce-other-page.js
@@ -2,16 +2,5 @@
 $('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
-$(document).ready(function () {
-    equalHeightAsTheTallest();
-});
 
-function equalHeightAsTheTallest() {
-    var heights = $(".product-box").map(function () {
-            return $(this).height();
-        }).get(),
-
-        maxHeight = Math.max.apply(null, heights);
-    $(".product-box").height(maxHeight);
-}
 
