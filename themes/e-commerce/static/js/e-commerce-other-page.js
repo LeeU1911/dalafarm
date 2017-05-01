a942f59f69ed8077ad95a74b671d1957d52203b5
Le Tan Vu <vu.letan@bosch-si.com>
2017-05-01 12:32:31 +0800

    Finish sub menu

--- themes/e-commerce/static/js/e-commerce-other-page.js
+++ themes/e-commerce/static/js/e-commerce-other-page.js
@@ -1,5 +1,8 @@
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function () {
+    if($(this).hasClass('dropdown-toggle')){
+        return;
+    }
     $('.navbar-toggle:visible').click();
 });
 
