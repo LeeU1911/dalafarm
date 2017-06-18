a5d00169ba776b4cd434c0a4b51b6bfe7a2a91f2
Le Tan Vu <vu.letan@bosch-si.com>
2017-06-18 15:41:57 +0800

    Add copy button and refactor js code

---
+++ themes/e-commerce/assets/js/e-commerce-order-pages.js
@@ -0,0 +1,32 @@
+$(function() {
+    // Initialize copy button
+    var clipboard = new Clipboard('#copyOrderContentBtn');
+
+    clipboard.on('success', function (e) {
+        setTooltip(e.trigger, 'Copied!');
+        hideTooltip(e.trigger);
+    });
+
+    clipboard.on('error', function (e) {
+        setTooltip(e.trigger, 'Failed!');
+        hideTooltip(e.trigger);
+    });
+    // Tooltip
+
+    $('#copyOrderContentBtn').tooltip({
+        trigger: 'click',
+        placement: 'bottom'
+    });
+
+    function setTooltip(btn, message) {
+        $(btn).tooltip('hide')
+            .attr('data-original-title', message)
+            .tooltip('show');
+    }
+
+    function hideTooltip(btn) {
+        setTimeout(function () {
+            $(btn).tooltip('hide');
+        }, 1000);
+    }
+});
