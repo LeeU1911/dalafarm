77ef3e8d02d25643d80e73c5a82b4efcd101b72a
Le Tan Vu <vu.letan@bosch-si.com>
2017-07-05 20:37:56 +0800

    Refactor code

--- themes/e-commerce/assets/js/e-commerce-order-pages.js
+++ themes/e-commerce/assets/js/e-commerce-order-pages.js
@@ -183,6 +183,18 @@ function applyPromotion(bill){
     }
     return bill;
 }
+
+// Utility methods
+function generateUID() {
+    // I generate the UID from two parts here
+    // to ensure the random number provide enough bits.
+    var firstPart = (Math.random() * 46656) | 0;
+    var secondPart = (Math.random() * 46656) | 0;
+    firstPart = ("000" + firstPart.toString(36)).slice(-3);
+    secondPart = ("000" + secondPart.toString(36)).slice(-3);
+    return firstPart + secondPart;
+}
+
 var Latinise = {};
 Latinise.latin_map = {
     "Á": "A",
