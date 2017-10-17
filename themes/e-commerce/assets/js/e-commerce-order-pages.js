aa4b357c9234c3ce901c7ba15f66180134264959
Le Tan Vu <vu.letan@bosch-si.com>
2017-10-17 11:08:09 +0800

    Fix promotion logic

--- themes/e-commerce/assets/js/e-commerce-order-pages.js
+++ themes/e-commerce/assets/js/e-commerce-order-pages.js
@@ -130,12 +130,12 @@ function calculateWeightOfPowders(bill) {
 }
 
 function applyPromotion(bill){
-    if(bill.promoCode != undefined && bill.promoCode != "") {
-        alert("Lưu ý: Đơn hàng của bạn không được hưởng chương trình khuyến mãi hiện có vì đang áp dụng một mã giảm giá khuyến mãi khác.");
-        return bill;
-    }
     var totalBillWoShippingCost = calculateTotalBillWithoutShippingCost(bill);
     bill.subtotal = totalBillWoShippingCost;
+	if(bill.promoCode != undefined && bill.promoCode != "") {
+        alert("Lưu ý: Đơn hàng của bạn không được hưởng chương trình khuyến mãi 20/10 hiện có vì đang áp dụng mã giảm giá khuyến mãi " + bill.promoCode + ".");
+        return bill;
+    }
     bill.promotionalProducts = {};
     var weight = calculateWeightOfPowders(bill);
     // if(weight >= 500 && only50gPowdersInOrder(bill)) {
